d09b58278d87347b058f5fa9a436c1ad7b4a6c45
zooiazainab <zooiazainab@gmail.com>
2024-01-08 13:38:58 +0500

    updated

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:3500',
-    specPattern: 'test/**/*.cy.{js,jsx,ts,tsx}',
-  },
-})
